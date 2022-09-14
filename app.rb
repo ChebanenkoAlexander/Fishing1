11274fcdf53bf8b1cbac535e4d989a7cf2793079
ChebanenkoAlexander <chebanenko56@gmail.com>
2022-09-15 00:34:05 +0500

    renamed pages

--- app.rb
+++ app.rb
@@ -4,5 +4,5 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 get '/' do
-	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
+	erb "Добрый день. Данный сайт находится в разработке! Приносим извинения за доставленные неудобства!"			
 end
